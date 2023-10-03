b4f22df1d1197069aec7806977d98a9a2442d8be
Okke van Eck <okke.vaneck@bsc.es>
2023-10-03 17:32:31 +0200

    New figures

--- benchmarking/visualize.py
+++ benchmarking/visualize.py
@@ -23,19 +23,19 @@ def plot_x_version(speedups, computer, compiler):
         "OpenMP_CPU": "OpenMP\nCPU",
         "OpenMP_GPU_Teams": "OpenMP\nGPU Teams",
         "Compute_OpenMP_GPU_Teams_Parallel": "OpenMP\nGPU Teams\nParallel",
-        "Compute_OpenMP_GPU_Teams_Parallel_Implicit_Map": "OpenMP\nGPU Teams\nParallel Implicit Map",
+        "Compute_OpenMP_GPU_Teams_Parallel_Implicit_Map": "OpenMP\nGPU Teams\nParallel\nImplicit Map",
         "Compute_OpenMP_GPU_Teams_Parallel_Data": "OpenMP\nGPU Teams\nParallel Data",
     }
     speedups["version"] = speedups["version"].map(version_trans)
 
-    plt.figure().gca()
+    plt.figure(figsize=(7,7)).gca()
     sns.lineplot(data=speedups, x="version", y="speedup", hue="size", palette="tab10", marker=".")
     plt.title(f"Speedups on {computer} using {compiler}")
     plt.savefig(f"figures/{computer}_{compiler}_version.png")
 
 
 def plot_x_size(speedups, computer, compiler):
-    ax = plt.figure().gca()
+    ax = plt.figure(figsize=(7,5)).gca()
     sns.scatterplot(data=speedups, x="size", y="speedup", hue="version", palette="tab10")
 
     # Style and show.
