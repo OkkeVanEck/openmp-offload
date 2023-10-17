b1ba2e4957d41a0054a3ea68ac522dbb5eced95a
Okke van Eck <okke.vaneck@bsc.es>
2023-10-17 17:05:40 +0200

    Added output files and slurm job script

--- Nemo/pull_lumi.sh
+++ Nemo/pull_lumi.sh
@@ -1,3 +1,3 @@
 rm -rf lumi_version
 mkdir lumi_version
-scp -r lumi:openmp-offload/Nemo/adv_x_mock/{*.F90,Makefile,results.*} ./lumi_version
+scp -r lumi:openmp-offload/Nemo/adv_x_mock/{*.F90,Makefile,results.*,*.txt,*.sh} ./lumi_version
